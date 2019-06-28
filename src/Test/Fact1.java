55a41dcec6315c483cbfcf4cecd00852b10f353b
ManikhandanS <sami.mani10@gmail.com>
2019-06-28 11:57:18 +0530

    Java Fact num

---
+++ src/Test/Fact1.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class Fact1 
+{
+public static void main(String[] args) 
+{
+int num=5, fac=1;
+for(int i=1;i<=num; ++i)
+{
+fac=fac*i;	
+}
+System.out.println("The Factorial of "+num+" is:  "+fac);
+}
+}
